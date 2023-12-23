2319a4b63b0cca123b83e6943cbae623dea2f6d3
Fabricio <fabriciolmd2308@gmail.com>
2023-12-23 17:02:24 -0300

    React 23/12

---
+++ React/react-foodApp/src/components/UI/Button.jsx
@@ -0,0 +1,10 @@
+/* eslint-disable react/prop-types */
+export default function Button({ children, textOnly, className, ...props }) {
+  const cssClasses = textOnly ? `text-button ${className}` : "button";
+
+  return (
+    <button {...props} className={cssClasses}>
+      {children}
+    </button>
+  );
+}
