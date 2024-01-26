5817d6bd91a9573793bd15b216b100f91ccb1e90
Fabricio <fabriciolmd2308@gmail.com>
2024-01-26 16:13:49 -0300

    Node 26/01

--- NodeJS/node-basics/server.js
+++ NodeJS/node-basics/server.js
@@ -1,6 +1,27 @@
+// Node core modules imports
 const http = require("http");
-const router = require("./routes");
+const path = require("path");
 
-const server = http.createServer(router);
+// 3rd party modules imports
+const express = require("express");
+const bodyParser = require("body-parser");
 
-server.listen(3000);
+// Custom imports
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+  res
+    .status(404)
+    .sendFile(path.join(__dirname, "views", "page-not-found.html"));
+});
+
+app.listen(3000);
