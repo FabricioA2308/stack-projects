01ef50bb57eb2fde8e14bde8cc290465e72ad32c
Fabricio <fabriciolmd2308@gmail.com>
2024-05-09 15:48:02 -0300

    added pagination

--- Python/python-postgre/app/routes/category_routes.py
+++ Python/python-postgre/app/routes/category_routes.py
@@ -1,12 +1,13 @@
 from typing import List
-from fastapi import APIRouter, Depends, Response, status
+from fastapi import APIRouter, Depends, Response, status, Query
 from sqlalchemy.orm import Session
 from app.schemas.category import Category, CategoryOutput
-from app.routes.deps import get_db_session
+from app.routes.deps import get_db_session, auth
 from app.use_cases.category import CategoryUseCases
+from fastapi_pagination import Page, add_pagination
 
 
-router = APIRouter(prefix='/category', tags=['Category'])
+router = APIRouter(prefix='/category', tags=['Category'], dependencies=[Depends(auth)])
 
 
 @router.post('/add', status_code=status.HTTP_201_CREATED, description="Add new category")
@@ -19,12 +20,14 @@ def add_category(
     return Response(status_code=status.HTTP_201_CREATED)
 
 
-@router.get('/list', response_model=List[CategoryOutput], description="List categories")
+@router.get('/list', response_model=Page[CategoryOutput], description="List categories")
 def list_categories(
+    page: int = Query(1, ge=1, description="Page number"),
+    size: int = Query(50, ge=1, le=100, description="Page size"),
     db_session: Session = Depends(get_db_session)
 ):
     uc = CategoryUseCases(db_session=db_session)
-    response = uc.list_categories()
+    response = uc.list_categories(page=page, size=size)
 
     return response
 
