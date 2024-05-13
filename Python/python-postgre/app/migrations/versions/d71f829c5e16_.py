9801987a7cb22cc6b33023dcd79962b24d6d3be9
Fabricio <fabriciolmd2308@gmail.com>
2024-05-13 15:35:52 -0300

    finished pagination

---
+++ Python/python-postgre/app/migrations/versions/d71f829c5e16_.py
@@ -0,0 +1,24 @@
+"""empty message
+
+Revision ID: d71f829c5e16
+Revises: 796186d2cd7a
+Create Date: 2024-05-13 18:26:41.410039
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = 'd71f829c5e16'
+down_revision = '796186d2cd7a'
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
