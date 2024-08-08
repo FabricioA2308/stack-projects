f1439cbca86503eadfad483466558eda21a86d7e
Fabricio <fabriciolmd2308@gmail.com>
2024-08-08 17:19:12 -0300

    2nd django project commit

--- Python/book_store/book_outlet/admin.py
+++ Python/book_store/book_outlet/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
+from .models import Book, Author
+
 # Register your models here.
+
+
+class BookAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title", )}
+    list_filter = ("author", "rating",)
+    list_display = ("title", "author",)
+
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+admin.site.register(Book, BookAdmin)
+admin.site.register(Author, AuthorAdmin)
