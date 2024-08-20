ce3e06fa806cd6dbb5401a09d1596443a9a8cd48
Fabricio <fabriciolmd2308@gmail.com>
2024-08-20 16:03:48 -0300

    connected blog to a sqlite db

--- Python/django_blog/blog/admin.py
+++ Python/django_blog/blog/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
 
+from .models import Tag, Author, Post
+
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    prepopulated_fields = {"slug": ("title", )}
+    list_filter = ("author", "caption", "date",)
+
+admin.site.register(Tag)
+admin.site.register(Author)
+admin.site.register(Post, PostAdmin)
